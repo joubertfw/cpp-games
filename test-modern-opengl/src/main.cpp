4be0be8ad0743c3cf5ad8b46cf8289b35c985a6c
Joubert Filho <joubert.fw@gmail.com>
2023-03-20 20:21:14 +0000

    Added vertex buffer example to main code

--- test-modern-opengl/src/main.cpp
+++ test-modern-opengl/src/main.cpp
@@ -18,6 +18,7 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    
     if (!window)
     {
         glfwTerminate();
@@ -33,16 +34,24 @@ int main(void)
         std::cout << "GLEW Error!" << std::endl;
     }
 
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] { 
+           -0.5f, -0.5f,
+            0.0f,  0.5f,
+            0.5f, -0.5f 
+    };
+    unsigned int buffer;
+
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        // glClear(GL_COLOR_BUFFER_BIT);
-        // glBegin(GL_TRIANGLES);
-        // glVertex2f(-0.5f, 0.5f);
-        // glVertex2f(0.0f, -0.5f);
-        // glVertex2f(0.5f, 0.5f);
-        // glEnd();
+        
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
