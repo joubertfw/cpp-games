11484f462f3a5c84cf450f0d8d9a2602e7a1b23e
Joubert Filho <joubert.fw@gmail.com>
2023-03-25 13:04:41 +0000

    Separating shader code in files

--- test-modern-opengl/src/main.cpp
+++ test-modern-opengl/src/main.cpp
@@ -3,6 +3,49 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+
+enum class ShaderType
+{
+    NONE = -1, 
+    VERTEX = 0, 
+    FRAGMENT = 1
+};
+
+struct Shader
+{
+    ShaderType type;
+    std::stringstream shader;
+};
+
+static Shader ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+    std::string line;
+
+    Shader data;
+
+    while(getline(stream, line))
+    {
+        if(line.find("#shader") != std::string::npos)
+        {
+            if(line.find("vertex"))
+            {
+                data.type = ShaderType::VERTEX;
+            }
+            else if(line.find("fragment"))
+            {
+                data.type = ShaderType::FRAGMENT;
+            }
+        }
+        else
+        {
+            data.shader << line << std::endl;
+        }
+    }
+
+    return data;
+}
 
 static int CompileShader(unsigned int type, const std::string& shader)
 {
@@ -97,20 +140,8 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-
-    std::string vertexShader = 
-    "#version 330 core\n"
-    "layout(location = 0) in vec4 position;\n"
-    "void main(){\n"
-    "gl_Position = position;\n"
-    "}\n";
-
-    std::string fragmentShader = 
-    "#version 330 core\n"
-    "layout(location = 0) out vec4 color;\n"
-    "void main(){\n"
-    "color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-    "}\n";
+    std::string vertexShader = ParseShader();
+    std::string fragmentShader = ParseShader();
 
     unsigned int shader = CreateShader(vertexShader, fragmentShader);
     glUseProgram(shader);
@@ -128,6 +159,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteShader();
 
     glfwTerminate();
     return 0;
