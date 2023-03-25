b6422350fd06dc0bdd9e65b8ab0d70269944676a
Joubert Filho <joubert.fw@gmail.com>
2023-03-25 13:13:31 +0000

    Fixing parse shader function

--- test-modern-opengl/res/shaders/vertexShader.shader
+++ test-modern-opengl/res/shaders/vertexShader.shader
@@ -1,4 +1,3 @@
-#shader vertex
 #version 330 core
 
 layout(location = 0) in vec4 position;
--- test-modern-opengl/src/main.cpp
+++ test-modern-opengl/src/main.cpp
@@ -5,46 +5,19 @@
 #include <iostream>
 #include <fstream>
 
-enum class ShaderType
-{
-    NONE = -1, 
-    VERTEX = 0, 
-    FRAGMENT = 1
-};
-
-struct Shader
-{
-    ShaderType type;
-    std::stringstream shader;
-};
-
-static Shader ParseShader(const std::string& filepath)
+static std::string ParseShader(const std::string& filepath)
 {
     std::ifstream stream(filepath);
     std::string line;
 
-    Shader data;
+    std::string shader;
 
     while(getline(stream, line))
     {
-        if(line.find("#shader") != std::string::npos)
-        {
-            if(line.find("vertex"))
-            {
-                data.type = ShaderType::VERTEX;
-            }
-            else if(line.find("fragment"))
-            {
-                data.type = ShaderType::FRAGMENT;
-            }
-        }
-        else
-        {
-            data.shader << line << std::endl;
-        }
+        shader += line + "\n";
     }
 
-    return data;
+    return shader;
 }
 
 static int CompileShader(unsigned int type, const std::string& shader)
@@ -140,8 +113,8 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-    std::string vertexShader = ParseShader();
-    std::string fragmentShader = ParseShader();
+    std::string vertexShader = ParseShader("res/shaders/vertexShader.shader");
+    std::string fragmentShader = ParseShader("res/shaders/fragmentShader.shader");
 
     unsigned int shader = CreateShader(vertexShader, fragmentShader);
     glUseProgram(shader);
@@ -159,8 +132,6 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
-    glDeleteShader();
 
     glfwTerminate();
     return 0;
