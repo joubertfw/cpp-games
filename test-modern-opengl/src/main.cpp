05e49fb082e54157329bf0111ebf920d6acfbb86
Joubert Filho <joubert.fw@gmail.com>
2023-03-25 13:28:10 +0000

    Added index buffer to draw square

--- test-modern-opengl/src/main.cpp
+++ test-modern-opengl/src/main.cpp
@@ -99,19 +99,32 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6] { 
+    float positions[] { 
            -0.5f, -0.5f,
-            0.0f,  0.5f,
-            0.5f, -0.5f 
+            0.5f, -0.5f,
+            0.5f,  0.5f,
+
+           -0.5f,  0.5f,
     };
+
+    unsigned int indices [] { 
+        0, 1, 2,
+        2, 3, 0
+    };
+
     unsigned int buffer;
 
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 *  sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     std::string vertexShader = ParseShader("res/shaders/vertexShader.shader");
     std::string fragmentShader = ParseShader("res/shaders/fragmentShader.shader");
@@ -124,7 +137,7 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
